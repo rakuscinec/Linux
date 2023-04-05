ef4fbe55d3192558472e864a31b4682ff288cace
rakuscinec <tomas@rakuscinec.eu>
2023-04-05 11:25:25 +0200

    Update install_openvas_22.4.0_ubuntu.sh

--- install_openvas_22.4.0_ubuntu.sh
+++ install_openvas_22.4.0_ubuntu.sh
@@ -591,7 +591,7 @@ Group=gvm
 RuntimeDirectory=gsad
 RuntimeDirectoryMode=2775
 PIDFile=/run/gsad/gsad.pid
-ExecStart=/usr/local/sbin/gsad --foreground --listen=0.0.0.0 --port=80 --http-only
+ExecStart=/usr/local/sbin/gsad --foreground --listen=0.0.0.0 -port=9392 --http-only
 Restart=always
 TimeoutStopSec=10
 
