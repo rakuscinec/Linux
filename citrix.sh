0a038b60ac54567631c083862cb0724b8dd16b78
rakuscinec <tomas@rakuscinec.eu>
2023-06-06 14:01:16 +0200

    Update citrix.sh

--- citrix.sh
+++ citrix.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-function screenIsLocked { [ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUsers:0:CGSSessionScreenIsLocked" /dev/stdin 2>/dev/null <<< "$(ioreg -n Root -d1 -a)")" = "true" ] && return 0 || return 1; }
-function screenIsUnlocked { [ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUsers:0:CGSSessionScreenIsLocked" /dev/stdin 2>/dev/null <<< "$(ioreg -n Root -d1 -a)")" != "true" ] && return 0 || return 1; }
+islocked=$(/usr/sbin/ioreg -n Root -d1 | grep IOConsoleLocked | awk -F \= '{print $2}')
 
-if screenIsUnlocked; then
-	if pgrep -x "Citrix Viewer" > /dev/null; then
-		echo "$(date) Citrix Running" >> /Users/tomas.rakuscinec/citrix.txt
-		osascript -e 'tell application "Keyboard Maestro Engine" to do script "688157E5-5312-4DF0-9CF4-A383920CF40C"'
-
-	fi
+if [ $islocked == "No" ]; then
+ echo "$(date) Pinging Citrix" >> /Users/tomas.rakuscinec/citrix.txt
+ osascript -e 'tell application "Keyboard Maestro Engine" to do script "688157E5-5312-4DF0-9CF4-A383920CF40C"'
+else
+ echo "$(date) Screen is Locked, not Pinging Citrix" | tee -a /Users/tomas.rakuscinec/citrix.txt
 fi
