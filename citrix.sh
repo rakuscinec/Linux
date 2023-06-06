3fe0220ec9d198ce3bdcbc266a3c2af8d73c2e50
rakuscinec <tomas@rakuscinec.eu>
2023-06-06 14:25:27 +0200

    Update citrix.sh

--- citrix.sh
+++ citrix.sh
@@ -3,8 +3,10 @@
 islocked=$(/usr/sbin/ioreg -n Root -d1 | grep IOConsoleLocked | awk -F \= '{print $2}')
 
 if [ $islocked == "No" ]; then
- echo "$(date) Pinging Citrix" >> /Users/tomas.rakuscinec/citrix.txt
- osascript -e 'tell application "Keyboard Maestro Engine" to do script "688157E5-5312-4DF0-9CF4-A383920CF40C"'
+ if pgrep -x "Citrix Viewer" > /dev/null; then
+  echo "$(date) Pinging Citrix" >> /Users/tomas.rakuscinec/citrix.txt
+  osascript -e 'tell application "Keyboard Maestro Engine" to do script "688157E5-5312-4DF0-9CF4-A383920CF40C"'
+ fi
 else
  echo "$(date) Screen is Locked, not Pinging Citrix" | tee -a /Users/tomas.rakuscinec/citrix.txt
 fi
